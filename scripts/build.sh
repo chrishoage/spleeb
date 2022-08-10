2f3332e0f7849287c31e46ac562da25d3a61c173
Chris Hoage <iam@chrishoage.com>
2022-08-09 18:11:51 -0700

    Address DRC errors with GND

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,4 @@ mkdir -p images
 docker run -u "$(id -u):$(id -g)" -w /board -v "$(pwd):/board" --rm yaqwsx/kikit:v1.0.5 pcbdraw --style builtin:set-black-cu.json build/pcbs/spleeb.kicad_pcb images/left.png
 docker run -u "$(id -u):$(id -g)" -w /board -v "$(pwd):/board" --rm yaqwsx/kikit:v1.0.5 pcbdraw --style builtin:set-black-cu.json -b build/pcbs/spleeb.kicad_pcb images/right.png
 
-cp -r build spleeb
+rsync -r build/ spleeb
--- scripts/fab.sh
+++ scripts/fab.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# DRC is failing here due to warnings in kicad of unconnnected nets, these are ground nets and don't need to be connected
-docker run -u "$(id -u):$(id -g)" -w /board -v "$(pwd):/board" --rm yaqwsx/kikit:v1.0.5 kikit fab jlcpcb --no-drc spleeb/pcbs/spleeb.kicad_pcb spleeb
+docker run -u "$(id -u):$(id -g)" -w /board -v "$(pwd):/board" --rm yaqwsx/kikit:v1.0.5 kikit fab jlcpcb spleeb/pcbs/spleeb.kicad_pcb spleeb
