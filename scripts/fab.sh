d9876c7cac05880c909740a75bac13a70c63cb59
Chris Hoage <iam@chrishoage.com>
2022-08-13 11:58:59 -0700

    Updates for plate including oled cutout

--- scripts/fab.sh
+++ scripts/fab.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -u "$(id -u):$(id -g)" -w /board -v "$(pwd):/board" --rm yaqwsx/kikit:v1.0.5 kikit fab jlcpcb spleeb/pcbs/spleeb.kicad_pcb spleeb
+docker run -u "$(id -u):$(id -g)" -w /board -v "$(pwd):/board" --rm yaqwsx/kikit:v1.0.5 kikit fab jlcpcb --nametemplate 'spleeb-{}' spleeb/pcbs/spleeb.kicad_pcb spleeb
+docker run -u "$(id -u):$(id -g)" -w /board -v "$(pwd):/board" --rm yaqwsx/kikit:v1.0.5 kikit fab jlcpcb --nametemplate 'plate-{}' spleeb/pcbs/plate.kicad_pcb spleeb
+rm -r spleeb/gerber
---
+++ scripts/step.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+kicad2step -o spleeb/pcbs/spleeb.step -f spleeb/pcbs/spleeb.kicad_pcb
+kicad2step -o spleeb/pcbs/plate.step -f spleeb/pcbs/plate.kicad_pcb
