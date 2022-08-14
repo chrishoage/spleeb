dec7eb564923e3ae7dae8f54b0272bd31500a802
Chris Hoage <iam@chrishoage.com>
2022-08-14 12:45:51 -0700

    load teardrop plugin and run with defaults before fill

--- scripts/fill.py
+++ scripts/fill.py
@@ -2,6 +2,7 @@
 
 import pcbnew
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('kicad_pcb')
@@ -9,7 +10,9 @@ args = parser.parse_args()
 
 file = args.kicad_pcb
 
+pcbnew.LoadPlugins(bundlepath=None, userpath=os.path.join(os.path.expanduser('~'), '.local/share/kicad/6.0/scripting'), thirdpartypath=None)
 board = pcbnew.LoadBoard(file)
+pcbnew.KICAD_PLUGINS["teardrops"]["ModuleName"].td.SetTeardrops(pcb=board)
 filler_tool = pcbnew.ZONE_FILLER(board)
 filler_tool.Fill(board.Zones())
 pcbnew.SaveBoard(file, board)
