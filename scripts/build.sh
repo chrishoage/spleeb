a742115fd2b439947c431c6563330591cb8625f3
Chris Hoage <iam@chrishoage.com>
2022-08-14 12:46:28 -0700

    move from docker to pipx for now and use fixed pcbdraw along with clean

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-node scripts/cli.js src/spleeb.yaml --output build --footprints src/footprints
+node scripts/cli.js src/spleeb.yaml --clean --output build --footprints src/footprints
+
+./scripts/fill.py build/pcbs/spleeb.kicad_pcb
 
 mkdir -p images
-docker run -u "$(id -u):$(id -g)" -w /board -v "$(pwd):/board" --rm yaqwsx/kikit:v1.0.5 pcbdraw --style builtin:set-white-cu.json build/pcbs/spleeb.kicad_pcb images/left.png
-docker run -u "$(id -u):$(id -g)" -w /board -v "$(pwd):/board" --rm yaqwsx/kikit:v1.0.5 pcbdraw --style builtin:set-white-cu.json -b build/pcbs/spleeb.kicad_pcb images/right.png
+pcbdraw --style builtin:set-white-cu.json build/pcbs/spleeb.kicad_pcb images/left.png
+pcbdraw --style builtin:set-white-cu.json -b build/pcbs/spleeb.kicad_pcb images/right.png
 
 rsync -r build/ spleeb
-
-./scripts/fill.py spleeb/pcbs/spleeb.kicad_pcb
